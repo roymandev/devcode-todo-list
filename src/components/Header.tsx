8b15d4511b13849a3f399ea8c02289e3c46bbf84
Royman <royman.dev@gmail.com>
2022-09-26 06:44:20 +0700

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => (
+  <header className="bg-primary flex h-[105px] items-center text-white">
+    <div className="mx-auto w-full max-w-[1000px] text-2xl font-bold">
+      <span>TO DO LIST APP</span>
+    </div>
+  </header>
+);
+
+export default Header;
