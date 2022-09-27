fdd674019c707c784b1d53154a9f884edb12df93
Royman <royman.dev@gmail.com>
2022-09-27 07:46:12 +0700

    feat: make Header position fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 const Header = () => (
-  <header className="bg-primary flex h-[105px] items-center text-white">
+  <header className="bg-primary fixed inset-0 flex h-[105px] items-center text-white [&+*]:mt-[105px]">
     <div className="mx-auto w-full max-w-[1000px] text-2xl font-bold">
       <span>TO DO LIST APP</span>
     </div>
