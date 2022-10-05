ed7b2ece3aaa76dfb5a223988193180e9c96fa9d
Royman <royman.dev@gmail.com>
2022-10-05 22:58:04 +0700

    refactor: rename constants

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,4 @@
-export const BASE_API_URL = 'https://todo.api.devcode.gethired.id';
 export const API_EMAIL_ENV = 'royman.dev@gmail.com';
+export const API_BASE_URL = 'https://todo.api.devcode.gethired.id';
+export const API_ACTIVITY_PATH = '/activity-groups';
+export const API_TODO_PATH = '/todo-items';
