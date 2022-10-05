57f1fdcd4e3d7b28b9d90d4d4f1e0dfa597f904a
Royman <royman.dev@gmail.com>
2022-10-05 23:01:16 +0700

    feat: add activity store

---
+++ src/stores/activityStore.ts
@@ -0,0 +1,56 @@
+import { API_ACTIVITY_PATH, API_EMAIL_ENV, API_BASE_URL } from '@/constant';
+import { promiseHanlder } from '@/lib/promiseHandler';
+import { atom } from 'jotai';
+
+export interface Activity {
+  id: number;
+  email: string;
+  title: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: null;
+}
+
+// State
+export const atomActivityList = atom<Activity[]>([]);
+
+// Actions
+export const atomFetchAllActivity = atom(null, async (get, set) => {
+  const [res] = await promiseHanlder(
+    fetch(API_BASE_URL + API_ACTIVITY_PATH + '?email=' + API_EMAIL_ENV),
+  );
+
+  if (res?.ok) {
+    const [resJson] = await promiseHanlder<{ data: Activity[] }>(res.json());
+
+    if (resJson) {
+      set(atomActivityList, resJson.data);
+      return;
+    }
+  }
+
+  throw Error('Failed to get activities');
+});
+
+export const atomCreateActivity = atom(null, async (get, set) => {
+  const [res] = await promiseHanlder(
+    fetch(API_BASE_URL + API_ACTIVITY_PATH, {
+      method: 'POST',
+      body: JSON.stringify({ title: 'New Activity', email: API_EMAIL_ENV }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }),
+  );
+
+  if (res?.ok) {
+    const [resJson] = await promiseHanlder<Activity>(res.json());
+
+    if (resJson) {
+      set(atomActivityList, (activities) => [resJson, ...activities]);
+      return;
+    }
+  }
+
+  throw Error('Failed to create new activity');
+});
