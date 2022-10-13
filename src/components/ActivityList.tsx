4762c0e7724e9aab09967e0e1e24c0e75aad8d31
Royman <royman.dev@gmail.com>
2022-10-13 08:09:52 +0700

    feat: add ActivityList component

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,28 +1,51 @@
 import Activity from '@/components/Activity';
-import { atomActivityList, atomCreateActivity } from '@/stores/activityStore';
+import {
+  Activity as ActivityType,
+  atomActivityList,
+  atomCreateActivity,
+} from '@/stores/activityStore';
 import { useAtomValue, useSetAtom } from 'jotai';
+import ModalDeleteActivity from '@/components/modals/DeleteActivity';
+import { useState } from 'react';
 
 const ActivityList = () => {
   const activities = useAtomValue(atomActivityList);
   const createActivity = useSetAtom(atomCreateActivity);
-
-  if (!activities.length)
-    return (
-      <button
-        data-cy="activity-empty-state"
-        className="mx-auto"
-        onClick={createActivity}
-      >
-        <img src="/activity-empty-state.png" alt="No activity" />
-      </button>
-    );
+  const [deleteActivity, setDeleteActivity] = useState<ActivityType | null>(
+    null,
+  );
 
   return (
-    <div className="grid grid-cols-4 gap-5">
-      {activities.map((activity, index) => (
-        <Activity index={index} key={activity.id} {...activity} />
-      ))}
-    </div>
+    <>
+      {activities.length ? (
+        <div className="grid grid-cols-4 gap-5">
+          {activities.map((activity, index) => (
+            <Activity
+              index={index}
+              key={activity.id}
+              onDelete={() => setDeleteActivity(activity)}
+              {...activity}
+            />
+          ))}
+        </div>
+      ) : (
+        <button
+          data-cy="activity-empty-state"
+          className="mx-auto"
+          onClick={createActivity}
+        >
+          <img src="/activity-empty-state.png" alt="No activity" />
+        </button>
+      )}
+
+      {deleteActivity && (
+        <ModalDeleteActivity
+          show={!!deleteActivity}
+          activity={deleteActivity}
+          onClose={() => setDeleteActivity(null)}
+        />
+      )}
+    </>
   );
 };
 
