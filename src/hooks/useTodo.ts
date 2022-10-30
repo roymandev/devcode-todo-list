78286fa7befae73357d89558cc27a7f551f1aba5
Royman <royman.dev@gmail.com>
2022-10-31 05:08:33 +0700

    chore: add useTodo hooks

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -17,6 +17,23 @@ export interface Todo {
 const useTodo = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  const fetchTodos = async (activityId: Todo['id']) => {
+    const [res] = await promiseHanlder(
+      fetch(API_BASE_URL + API_TODO_PATH + '?activity_group_id=' + activityId),
+    );
+
+    if (res?.ok) {
+      const [resJson] = await promiseHanlder<{ data: Todo[] }>(res.json());
+
+      if (resJson) {
+        setTodos(resJson.data);
+        return;
+      }
+    }
+
+    throw Error('Failed to get todos');
+  };
+
   const createTodo = async (
     activityId: Activity['id'],
     title: Todo['title'],
@@ -45,10 +62,55 @@ const useTodo = () => {
       }
     }
 
-    throw Error('Failed to create new activity');
+    throw Error('Failed to create new todo');
   };
 
-  return { todos, setTodos, createTodo };
+  const updateTodo = async (
+    todoId: Todo['id'],
+    update: Pick<Todo, 'title' | 'is_active' | 'priority'>,
+  ) => {
+    const [res] = await promiseHanlder(
+      fetch(API_BASE_URL + API_TODO_PATH + '/' + todoId, {
+        method: 'PATCH',
+        body: JSON.stringify(update),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    );
+
+    if (res?.ok) {
+      const [resJson] = await promiseHanlder<Activity>(res.json());
+
+      if (resJson) {
+        setTodos((todos) =>
+          todos.map((todo) =>
+            todo.id == todoId ? { ...todo, ...resJson } : todo,
+          ),
+        );
+      }
+      return;
+    }
+
+    throw Error('Failed to update todo with id ' + todoId);
+  };
+
+  const deleteTodo = async (todoId: Todo['id']) => {
+    const [res] = await promiseHanlder(
+      fetch(API_BASE_URL + API_TODO_PATH + '/' + todoId, {
+        method: 'DELETE',
+      }),
+    );
+
+    if (res?.ok) {
+      setTodos((todos) => todos.filter((todo) => todo.id !== todoId));
+      return;
+    }
+
+    throw Error('Failed to delete activity with id ' + todoId);
+  };
+
+  return { todos, setTodos, fetchTodos, createTodo, updateTodo, deleteTodo };
 };
 
 export default useTodo;
