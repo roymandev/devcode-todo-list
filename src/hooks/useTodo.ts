da1fd653638b6d2f3eb66b411a5bbcc80dc9d557
Royman <royman.dev@gmail.com>
2022-11-10 05:28:06 +0700

    refactor: change useTodo methods

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -1,130 +1,55 @@
-import { API_BASE_URL, API_TODO_PATH } from '@/constant';
-import { promiseHanlder } from '@/lib/promiseHandler';
 import { sortTodos, TODOS_SORT_TYPE } from '@/lib/sortTodos';
+import { createTodo, deleteTodo, getTodos, Todo, updateTodo } from '@/lib/todo';
 import { Activity } from '@/store';
 import { useEffect, useState } from 'react';
 
-export interface Todo {
-  id: number;
-  title: string;
-  activity_group_id: number;
-  is_active: boolean;
-  priority: 'very-high' | 'high' | 'normal' | 'low' | 'very-low';
-}
-
-const useTodo = () => {
+const useTodo = (activityId: Activity['id']) => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [sortType, setSortType] = useState<TODOS_SORT_TYPE>('latest');
+
+  useEffect(() => {
+    fetchTodos();
+  }, [activityId]);
 
   useEffect(() => {
     if (todos.length) setTodos((prevTodos) => sortTodos(sortType, prevTodos));
   }, [sortType]);
 
-  const fetchTodos = async (activityId: Todo['id']) => {
-    const [res] = await promiseHanlder(
-      fetch(API_BASE_URL + API_TODO_PATH + '?activity_group_id=' + activityId),
-    );
+  const fetchTodos = () => getTodos(activityId).then(setTodos);
 
-    if (res?.ok) {
-      const [resJson] = await promiseHanlder<{ data: Todo[] }>(res.json());
+  const addTodo: typeof createTodo = (activityId, title, priority) =>
+    createTodo(activityId, title, priority).then(() => fetchTodos());
 
-      if (resJson) {
-        setTodos(resJson.data);
-        return;
-      }
+  const removeTodo = (todoId: Todo['id']) =>
+    deleteTodo(todoId).then(() => fetchTodos());
+
+  const setTodo: typeof updateTodo = (todoId, update) =>
+    updateTodo(todoId, update).then(fetchTodos);
+
+  const toggleTodo = (todoId: Todo['id']) => {
+    const todo = todos.find((todo) => todo.id === todoId);
+
+    if (todo) {
+      const newTodo = { ...todo, is_active: !todo.is_active };
+
+      updateTodo(todoId, newTodo);
+
+      setTodos((todos) =>
+        todos.map((todo) => (todo.id === todoId ? newTodo : todo)),
+      );
     }
-
-    throw Error('Failed to get todos');
-  };
-
-  const createTodo = async (
-    activityId: Activity['id'],
-    title: Todo['title'],
-    priority: Todo['priority'],
-  ) => {
-    const [res] = await promiseHanlder(
-      fetch(API_BASE_URL + API_TODO_PATH, {
-        method: 'POST',
-        body: JSON.stringify({
-          activity_group_id: activityId,
-          title,
-          priority,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }),
-    );
-
-    if (res?.ok) {
-      const [resJson] = await promiseHanlder<Todo>(res.json());
-
-      if (resJson) {
-        setTodos((todos) => sortTodos(sortType, [resJson, ...todos]));
-        return;
-      }
-    }
-
-    throw Error('Failed to create new todo');
-  };
-
-  const updateTodo = async (
-    todoId: Todo['id'],
-    update: Pick<Todo, 'title' | 'is_active' | 'priority'>,
-  ) => {
-    const [res] = await promiseHanlder(
-      fetch(API_BASE_URL + API_TODO_PATH + '/' + todoId, {
-        method: 'PATCH',
-        body: JSON.stringify(update),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }),
-    );
-
-    if (res?.ok) {
-      const [resJson] = await promiseHanlder<Activity>(res.json());
-
-      if (resJson) {
-        setTodos((todos) =>
-          sortTodos(
-            sortType,
-            todos.map((todo) =>
-              todo.id == todoId ? { ...todo, ...resJson } : todo,
-            ),
-          ),
-        );
-      }
-      return;
-    }
-
-    throw Error('Failed to update todo with id ' + todoId);
-  };
-
-  const deleteTodo = async (todoId: Todo['id']) => {
-    const [res] = await promiseHanlder(
-      fetch(API_BASE_URL + API_TODO_PATH + '/' + todoId, {
-        method: 'DELETE',
-      }),
-    );
-
-    if (res?.ok) {
-      setTodos((todos) => todos.filter((todo) => todo.id !== todoId));
-      return;
-    }
-
-    throw Error('Failed to delete activity with id ' + todoId);
   };
 
   return {
     todos,
     setTodos,
     fetchTodos,
-    createTodo,
-    updateTodo,
-    deleteTodo,
+    addTodo,
+    removeTodo,
+    setTodo,
     sortType,
     setSortType,
+    toggleTodo,
   };
 };
 
