85f2a9384e8dbb7d33ee3cc40f5cf8c1a560e16f
royman.dev <royman.dev@gmail.com>
2024-12-20 20:58:14 +0700

    feat: clean project

--- src/constant.ts
+++
@@ -1,4 +0,0 @@
-export const API_EMAIL_ENV = 'royman.dev@gmail.com';
-export const API_BASE_URL = 'https://todo.api.devcode.gethired.id';
-export const API_ACTIVITY_PATH = '/activity-groups';
-export const API_TODO_PATH = '/todo-items';
--- src/lib/formatDate.ts
+++
@@ -1,6 +0,0 @@
-export const formatDate = (date: string | number | Date) =>
-  new Date(date).toLocaleDateString('id-ID', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
