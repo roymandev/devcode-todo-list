0b8bd8eb25f02b84126ca3d259af17466ca4fb52
royman.dev <royman.dev@gmail.com>
2024-12-25 03:19:31 +0700

    ci: fix package bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
 });
